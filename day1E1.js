32176905c306e350fc8f01054a4198a9d52a8343
Rohan-DesunTech <meraj.desuntech@gmail.com>
2023-12-17 16:04:44 +0530

    first commit

---
+++ day1E1.js
@@ -0,0 +1,84 @@
+// Data types
+
+// String stringDAtatype = "sunil kumar"
+// Int integerdatatype = 10
+// Float floatDatatpe = 10.23
+// Boolean boleanDatatype = true / false
+
+// var , let , const - scoping
+
+// const thisIsconstantdatatyprWithString = "this is string";
+// const thisIsconstantdatatyprWithNumber = 10;
+// const thisIsconstantdatatyprWithFloat = 10.65;
+// const thisIsconstantdatatyprWithBoolean = true;
+
+
+// thisIsconstantdatatyprWithNumber = 20; // Error - (TypeError): Assignment to constant variable.
+
+// const thisIsconstantdatatyprWithNumber = 20; // Error - (SyntaxError): Identifier 'thisIsconstantdatatyprWithNumber' has already been declared
+
+
+// let thisIsletantdatatyprWithString = "this is string";
+// let thisIsletantdatatyprWithNumber = 10;
+// let thisIsletantdatatyprWithFloat = 10.65;
+// let thisIsletantdatatyprWithBoolean = true;
+
+// // thisIsletantdatatyprWithNumber = 20; // No Error in the case of Reassign
+
+// let thisIsletantdatatyprWithNumber = 20; // Error - (SyntaxError): Identifier 'thisIsletantdatatyprWithNumber' has already been declared
+
+
+var thisIsvarantdatatyprWithString = "this is string";
+var thisIsvarantdatatyprWithNumber = 10;
+var thisIsvarantdatatyprWithFloat = 10.65;
+var thisIsvarantdatatyprWithBoolean = true;
+
+// thisIsvarantdatatyprWithNumber = 20; // No Error in the case of Reassign
+
+// console.log(thisIsvarantdatatyprWithNumber);
+
+// var thisIsvarantdatatyprWithNumber = 30; // No Error in the case of Reassign
+
+// console.log(thisIsvarantdatatyprWithNumber);
+
+const backendResponse = [
+{
+    age: 26,
+    name:'sunil',
+    number: '6589745896',
+    isAdult: true,   
+},
+{
+    age: 19,
+    name:'rohan',
+    number: '6589745656',
+    isAdult: true,   
+},
+{
+    age: 15,
+    name:'kunal',
+    number: '6596745896',
+    isAdult: false,   
+},
+{
+    age: "No Idea",
+    name:'Divakar',
+    number: '9889745896',
+    isAdult: false,   
+},
+{
+    age: 32,
+    name:'Chitur',
+    number: '6289745656',
+    isAdult: true,   
+},
+{
+    age: '',
+    name:'Planav ful',
+    number: '9685745656',
+    isAdult: true,   
+},
+]
+
+
+console.log(backendResponse)
